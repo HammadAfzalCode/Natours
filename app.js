963010bd77a2a54456712f2b4c039ae35e17ca57
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-09-16 15:15:26 +0500

    added create tours api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json()); // middleware to grab data from req body otherwise it will be undefined
 const port = 3000;
 
 const tours = JSON.parse(fs.readFileSync(`./dev-data/data/tours-simple.json`));
@@ -16,6 +17,30 @@ app.get('/api/v1/tours', (req, res) => {
             tours,
         },
     });
+});
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign(
+        {
+            id: newId,
+        },
+        req.body
+    );
+    tours.push(newTour);
+
+
+    fs.writeFile(
+        `./dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        (err) => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour,
+                },
+            });
+        }
+    );
 });
 app.listen(port, () => {
     console.log(`App is listening of port: ${port}`);
