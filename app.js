89733194d3cf3e31408e6689a2a28efd00f57a0e
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-09-16 15:47:43 +0500

    added get tour using id through params

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const port = 3000;
 const tours = JSON.parse(fs.readFileSync(`./dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
+
     // here the cb is route Hanlder
     res.status(200).json({
         status: 'success',
@@ -28,7 +29,6 @@ app.post('/api/v1/tours', (req, res) => {
     );
     tours.push(newTour);
 
-
     fs.writeFile(
         `./dev-data/data/tours-simple.json`,
         JSON.stringify(tours),
@@ -41,6 +41,25 @@ app.post('/api/v1/tours', (req, res) => {
             });
         }
     );
+});
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params)
+    const id = req.params.id * 1
+
+    const tour = tours.find(el => el.id === id)
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: "Invalid Id"
+        })
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour,
+        },
+    });
+
 });
 app.listen(port, () => {
     console.log(`App is listening of port: ${port}`);
