b3eeb6875422b37d5e3ae1e64b7e897bfb287a85
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-09-16 17:57:05 +0500

    added morgan logger middleware

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // const ds = require(`${__dirname}/dev-data/data/tours-simple.json`)
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express();
+
+// Middlewares
+app.use(morgan('dev'))
 app.use(express.json()); // middleware to grab data from req body otherwise it will be undefined
 app.use((req, res, next) => {
     console.log('hello from middleware')
@@ -10,7 +14,6 @@ app.use((req, res, next) => {
 })
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString()
-
     next()
 })
 const port = 3000;
