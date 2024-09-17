6c8fa1ffea7c52f0e86bce33338e6134508e7a9d
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-09-17 20:08:35 +0500

    added separate folders for controllers,routes and also server.js

---
+++ routes/tourRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const tourController = require("../controlllers/tourController")
+const router = express.Router();
+const { getAllTours, createTour, getTour, updateTour, deleteTour } = tourController
+router.route('/').get(getAllTours).post(createTour);
+router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+module.exports = router
