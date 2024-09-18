f014b5299086aa07007751f3281ae32662454741
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-09-18 11:29:53 +0500

    serving static files

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+// const {} = require('./public')
 
 const app = express();
 
 // Middlewares
 app.use(morgan('dev'));
-app.use(express.json()); // middleware to grab data from req body otherwise it will be undefined
+app.use(express.json());
+app.use(express.static('./public')) // middleware to grab data from req body otherwise it will be undefined
 app.use((req, res, next) => {
     console.log('hello from middleware');
     next();
