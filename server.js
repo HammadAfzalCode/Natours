bbd997699aadd5ea5485ea7820b28716d7aec3cb
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-09-18 12:16:13 +0500

    added config.env

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const dotenv = require("dotenv")
+dotenv.config({
+    path: "./config.env"
+})
 const app = require("./app");
+console.log(process.env.NODE_ENV)
 
 
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
     console.log(`App is listening of port: ${port}`);
